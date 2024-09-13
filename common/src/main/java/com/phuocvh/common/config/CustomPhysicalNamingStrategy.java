ebdbaa9b965f84489f9bdff5ca53c8be4d226960
vhphuoc1102 <phuocvu511@gmail.com>
2024-09-13 07:58:45 +0700

    feat: Change naming strategy

--- common/src/main/java/com/phuocvh/common/config/CustomPhysicalNamingStrategy.java
+++ common/src/main/java/com/phuocvh/common/config/CustomPhysicalNamingStrategy.java
@@ -1,46 +1,50 @@
 package com.phuocvh.common.config;
 
+import java.util.Locale;
 import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 
-import java.util.Locale;
-
 public class CustomPhysicalNamingStrategy implements PhysicalNamingStrategy {
   @Override
-  public Identifier toPhysicalCatalogName(final Identifier identifier, final JdbcEnvironment jdbcEnv) {
+  public Identifier toPhysicalCatalogName(
+      final Identifier identifier, final JdbcEnvironment jdbcEnv) {
     return convertToSnakeCase(identifier);
   }
 
   @Override
-  public Identifier toPhysicalColumnName(final Identifier identifier, final JdbcEnvironment jdbcEnv) {
+  public Identifier toPhysicalColumnName(
+      final Identifier identifier, final JdbcEnvironment jdbcEnv) {
     return convertToSnakeCase(identifier);
   }
 
   @Override
-  public Identifier toPhysicalSchemaName(final Identifier identifier, final JdbcEnvironment jdbcEnv) {
+  public Identifier toPhysicalSchemaName(
+      final Identifier identifier, final JdbcEnvironment jdbcEnv) {
     return convertToSnakeCase(identifier);
   }
 
   @Override
-  public Identifier toPhysicalSequenceName(final Identifier identifier, final JdbcEnvironment jdbcEnv) {
+  public Identifier toPhysicalSequenceName(
+      final Identifier identifier, final JdbcEnvironment jdbcEnv) {
     return convertToSnakeCase(identifier);
   }
 
   @Override
-  public Identifier toPhysicalTableName(final Identifier identifier, final JdbcEnvironment jdbcEnv) {
+  public Identifier toPhysicalTableName(
+      final Identifier identifier, final JdbcEnvironment jdbcEnv) {
     return convertToSnakeCase(identifier);
   }
 
   private Identifier convertToSnakeCase(final Identifier identifier) {
-    if(identifier == null) {
+    if (identifier == null) {
       return null;
     }
     StringBuilder builder = new StringBuilder(identifier.getText().replace('.', '_'));
 
     for (int i = 1; i < builder.length() - 1; i++) {
       if (this.isUnderscoreRequired(
-              builder.charAt(i - 1), builder.charAt(i), builder.charAt(i + 1))) {
+          builder.charAt(i - 1), builder.charAt(i), builder.charAt(i + 1))) {
         builder.insert(i++, '_');
       }
     }
@@ -50,7 +54,7 @@ public class CustomPhysicalNamingStrategy implements PhysicalNamingStrategy {
 
   private boolean isUnderscoreRequired(char before, char current, char after) {
     return Character.isLowerCase(before)
-            && Character.isUpperCase(current)
-            && Character.isLowerCase(after);
+        && Character.isUpperCase(current)
+        && Character.isLowerCase(after);
   }
 }
